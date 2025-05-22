ef57172e2dcf47ab9839e3994c930804d5092a1a
L145 <147063982+legelff@users.noreply.github.com>
2025-05-22 13:44:03 +0200

    revert mistake

--- frontend/src/screens/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './LoginScreen';
-export * from './RegisterScreen';
-export * from './CreateProfileFlowScreen';
-export * from './AddEditPetScreen';
-// TODO: Export other screens as they are created
-// export * from './HomeOwnerScreen';
-// export * from './HomeSitterScreen';
-// etc.
